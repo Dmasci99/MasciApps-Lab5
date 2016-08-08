e13219c97f1922c4fa7a8643fc7c6b740baaa8a7
Dan Masci <minimasci@yahoo.ca>
2016-08-08 16:44:29 -0400

    Add project files.

---
+++ MasciApps-Lab5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MasciApps_Lab5.MvcApplication" Language="C#" %>
---
+++ MasciApps-Lab5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasciApps_Lab5.Startup))]
+namespace MasciApps_Lab5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
